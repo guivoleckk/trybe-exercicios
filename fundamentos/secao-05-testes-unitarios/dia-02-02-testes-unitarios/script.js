bd958d1218e0d51b32f3de8ec21ee7a38c4c031b
Guilherme <guivoleckk@hotmail.com>
2023-01-20 19:01:41 -0300

    ex feitos

---
+++ fundamentos/secao-05-testes-unitarios/dia-02-02-testes-unitarios/script.js
@@ -0,0 +1,111 @@
+function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
+    }
+    return newArr;
+  }
+  
+  // implemente seus testes aqui
+
+
+function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
+  }
+  
+  // implemente seus testes aqui
+
+
+
+
+// outra funcao 
+
+const mapString = (objectMap, string) => {
+  const splitString = string.split('');
+  const mappedArray = [];
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+    const mappedValue = objectMap[character];
+    
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
+    }
+  }
+  return mappedArray.join('');
+}
+const encode = (string) => {
+  const map = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return mapString(map, string);
+}
+const decode = (string) => {
+  const map = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return mapString(map, string);
+}
+
+// techList.js
+const techList = (arrayTechnologies, name) => {
+  if (arrayTechnologies.length === 0) return 'Vazio!';
+
+  const sortedArray = arrayTechnologies.sort();
+  const technologyList = [];
+
+  for (let index = 0; index < sortedArray.length; index += 1) {
+    technologyList.push({
+      tech: sortedArray[index],
+      name,
+    });
+  }
+
+  return technologyList;
+};
+
+
+//funcao 
+
+// hydrate.js
+const hydrate = (string) => {
+  const splitString = string.split('');
+  let glassesOfWater = 0;
+
+  for (let index = 0; index < splitString.length; index += 1) {
+    const parsedCharacter = parseInt(splitString[index]);
+
+    if (parsedCharacter) {
+      glassesOfWater += parsedCharacter;
+    }
+  }
+
+  let glass = 'copo';
+
+  if (glassesOfWater > 1) {
+    glass = 'copos';
+  }
+
+  return `${glassesOfWater} ${glass} de água`;
+};
+
+module.exports = hydrate;
+
+
+
+module.exports = {myRemove, myFizzBuzz, encode, decode, techList, hydrate };
